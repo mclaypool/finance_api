ff574490c9e90e2e23be93e73bfb5fef333f739b
max <mclay400@gmail.com>
2018-01-15 10:22:50 -0600

    Set up Flask AppBuilder

---
+++ finance_api/app/views.py
@@ -0,0 +1,29 @@
+from flask import render_template
+from flask.ext.appbuilder.models.sqla.interface import SQLAInterface
+from flask.ext.appbuilder import ModelView
+from app import appbuilder, db
+
+"""
+    Create your Views::
+
+
+    class MyModelView(ModelView):
+        datamodel = SQLAInterface(MyModel)
+
+
+    Next, register your Views::
+
+
+    appbuilder.add_view(MyModelView, "My View", icon="fa-folder-open-o", category="My Category", category_icon='fa-envelope')
+"""
+
+"""
+    Application wide 404 error handler
+"""
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
+
+db.create_all()
+
+
